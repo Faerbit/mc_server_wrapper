8c277d3e39564fbbbe7d45ce6572e5f7889ad94d
Faerbit <faerbit@server.fake>
2013-07-06 15:33:14 +0200

    Initial Commit

---
+++ minecraft_args.py
@@ -0,0 +1,37 @@
+import minecraft_startup as mc
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("action", choices=['start', 'stop', 'update', 'restart', 'backup', 'check_players', 'status', 'switch', 'ramdisk_saverun', 'command', 'config_test'])
+parser.add_argument("command", nargs='?')
+args=parser.parse_args()
+if args.action=='start':
+	mc.start()
+if args.action=="stop":
+	mc.stop()
+if args.action=="update":
+	mc.update()
+if args.action=="restart":
+	mc.restart()
+if args.action=="backup":
+	mc.backup("regular")
+if args.action=="check_players":
+	mc.check_players()
+if args.action=="status":
+	mc.status()
+if args.action=="switch":
+	if args.command==None:
+		print ("Please enter a minecraft variant!")
+	else:
+		mc.switch(args.command)
+if args.action=="ramdisk_saverun":
+	mc.ramdisk_saverun()
+if args.action=="command":
+	if args.command==None:
+		print ("Please enter a command!")
+	elif args.command=="stop":
+		print ("Please use the stop method provided by this script!")
+	else:
+		mc.command(args.command)
+if args.action=="config_test":
+	mc.config_test()
